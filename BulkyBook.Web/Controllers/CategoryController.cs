346ecfb97868edc1f02753dee476140f18a7c96b
Snoozebugs92 <100491452+Snoozebugs92@users.noreply.github.com>
2023-07-23 14:21:17 +0200

    Adding list of Category to DbContext

--- BulkyBook.Web/Controllers/CategoryController.cs
+++ BulkyBook.Web/Controllers/CategoryController.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyBook.Web.Data;
+using BulkyBook.Web.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBook.Web.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
+            List<Category> objCategoryList = _db.Categories.ToList();
             return View();
         }
     }
