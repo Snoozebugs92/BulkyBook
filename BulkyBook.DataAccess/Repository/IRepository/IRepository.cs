2be75d18f2e7c43576e8bba546f4808a5aa511a2
Snoozebugs92 <100491452+Snoozebugs92@users.noreply.github.com>
2023-07-24 16:48:40 +0200

    70. Implement Repository Interface

--- BulkyBook.DataAccess/Repository/IRepository/IRepository.cs
+++ BulkyBook.DataAccess/Repository/IRepository/IRepository.cs
@@ -1,11 +1,11 @@
 ﻿using System.Linq.Expressions;
 
-namespace BulkyBook.DataAccess.Repository
+namespace BulkyBook.DataAccess.Repository.IRepository
 {
     public interface IRepository<T> where T : class
     {
         IEnumerable<T> GetAll();
-        T Get(Expression<Func<T,bool>> filter);
+        T Get(Expression<Func<T, bool>> filter);
         void Add(T entity);
         void Remove(T entity);
         void RemoveRange(IEnumerable<T> entity);
