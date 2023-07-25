2799695b9cfcc4e1412e6ef0e5c5f23ab1bc8c0a
Snoozebugs92 <100491452+Snoozebugs92@users.noreply.github.com>
2023-07-25 15:35:19 +0200

    94. Create Product
    Adding image upload to product create

--- BulkyBook.Web/Areas/Admin/Controllers/ProductController.cs
+++ BulkyBook.Web/Areas/Admin/Controllers/ProductController.cs
@@ -11,10 +11,12 @@ namespace BulkyBook.Web.Areas.Admin.Controllers
     public class ProductController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public ProductController(IUnitOfWork unitOfWork)
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public IActionResult Index()
@@ -62,6 +64,18 @@ namespace BulkyBook.Web.Areas.Admin.Controllers
         {
             if (ModelState.IsValid)
             {
+                string wwwRootPath = _webHostEnvironment.WebRootPath;
+                if (file != null)
+                {
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    string productPath = Path.Combine(wwwRootPath, @"images\product");
+
+                    using (FileStream fs = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
+                    {
+                        file.CopyTo(fs);
+                    };
+                    productViewModel.Product.ImageUrl = @"\images\product\" + fileName;
+                }
                 _unitOfWork.Product.Add(productViewModel.Product);
                 _unitOfWork.Save();
                 TempData["success"] = "Product created successfully.";
