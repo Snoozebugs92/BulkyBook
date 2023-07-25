e809552c8877bf6e269175d75dcf2524f7536b8a
Snoozebugs92 <100491452+Snoozebugs92@users.noreply.github.com>
2023-07-25 13:13:09 +0200

    Revert "90. ViewModels in Action WIP"
    This reverts commit 65fdb1578bff5f5be3123dd4d3b801f4b882ec70.

--- BulkyBook.Web/Areas/Admin/Controllers/ProductController.cs
+++ BulkyBook.Web/Areas/Admin/Controllers/ProductController.cs
@@ -1,6 +1,8 @@
 ﻿using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
 
 namespace BulkyBook.Web.Areas.Admin.Controllers
 {
@@ -21,7 +23,17 @@ namespace BulkyBook.Web.Areas.Admin.Controllers
         }
 
         public IActionResult Create()
-        { 
+        {
+            IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category
+                .GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString(),
+                });
+
+            //ViewBag.CategoryList = CategoryList;
+            ViewData["CategoryList"] = CategoryList;
+
             return View();
         }
 
